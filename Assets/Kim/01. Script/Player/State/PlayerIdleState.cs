285ba4ad8ab7245ff405acfe41ce12d085e15707
meo-young <meoyoung000@naver.com>
2024-11-01 07:42:15 +0900

    사운드전까지 구현

--- Assets/Kim/01. Script/Player/State/PlayerIdleState.cs
+++ Assets/Kim/01. Script/Player/State/PlayerIdleState.cs
@@ -3,15 +3,22 @@
 public class PlayerIdleState : MonoBehaviour, IPlayerState
 {
     PlayerController pc;
+    private float startTimer;
     public void OnStateEnter(PlayerController controller)
     {
         if (pc == null)
             pc = controller;
+
+        startTimer = 0;
     }
 
     public void OnStateUpdate()
     {
-        pc.ChangeState(pc._moveState);
+        if (startTimer > 2)
+        {
+            pc.ChangeState(pc._moveState);
+        }
+        startTimer += Time.deltaTime;
     }
 
     public void OnStateExit()
