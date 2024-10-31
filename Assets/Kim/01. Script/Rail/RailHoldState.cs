59b44ed08d32765ec1f381e15c83264b5ed9587d
meo-young <meoyoung000@naver.com>
2024-11-01 07:24:39 +0900

    abc

--- Assets/Kim/01. Script/Rail/RailHoldState.cs
+++ Assets/Kim/01. Script/Rail/RailHoldState.cs
@@ -4,6 +4,7 @@ public class RailHoldState : MonoBehaviour, IRailState
 {
     RailController rc;
     Vector3 railUnderPos;
+    float holdingTIme;
 
     public void OnStateEnter(RailController controller)
     {
@@ -11,11 +12,18 @@ public class RailHoldState : MonoBehaviour, IRailState
             rc = controller;
 
         rc.animator.SetBool("Hold", true);
+        holdingTIme = 0;
         rc.playerController.ChangeState(rc.playerController._holdState);
     }
 
     public void OnStateUpdate()
     {
+        if(holdingTIme > 1.8)
+        {
+            rc.ChangeState(rc._exitState);
+            return;
+        }
+        holdingTIme += Time.deltaTime;
         rc.rb.linearVelocityX = rc.playerController.stat.moveSpeed - 0.2f;
         railUnderPos = new(rc.smagaeSpawnPoint.position.x, rc.smagaeSpawnPoint.position.y, 0);
         rc.playerController.transform.position = railUnderPos;
