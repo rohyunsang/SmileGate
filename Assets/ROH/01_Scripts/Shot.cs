61c9a34ae8ef3f3dda57d6e2881f98a31ab4676e
meo-young <meoyoung000@naver.com>
2024-11-01 07:32:43 +0900

    Shot 버그 수정

--- Assets/ROH/01_Scripts/Shot.cs
+++ Assets/ROH/01_Scripts/Shot.cs
@@ -15,24 +15,11 @@ public class Shot : MonoBehaviour
         if (!controller.CheckAvailableState())
             return;
 
-#if UNITY_EDITOR
         // 마우스 클릭 시
         if (Input.GetMouseButtonDown(0))
-#else
-        // 터치 시
-        if (Input.touchCount > 0)
-#endif
         {
-
-#if UNITY_EDITOR
+            // 마우스 클릭 위치를 저장
             m_vecMouseDownPos = Input.mousePosition;
-#else
-            m_vecMouseDownPos = Input.GetTouch(0).position;
-            if(Input.GetTouch(0).phase != TouchPhase.Began)
-                return;
-#endif
-            // if( FindObjectOfType<Player>().iSShot == false ) return;
-            Debug.Log("HELL");
 
             // 마우스 클릭 위치를 카메라 스크린 월드포인트로 변경합니다.
             Vector2 pos = Camera.main.ScreenToWorldPoint(m_vecMouseDownPos);
